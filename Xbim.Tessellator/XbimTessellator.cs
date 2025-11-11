e59e4a0401477965a66302cedef7215725b964b2
Renato Silveira (LT) <renatos@leapthought.co.nz>
2025-11-11 13:10:58 -0300

    - Set intersection vertex data to -1 when combine callback is null, ensuring they are properly added to the mesh later

--- Xbim.Tessellator/XbimTessellator.cs
+++ Xbim.Tessellator/XbimTessellator.cs
@@ -599,7 +599,7 @@ namespace Xbim.Tessellator
                     {
                         tess.AddContours(contours);
 
-                        tess.Tessellate(WindingRule.EvenOdd, ElementType.Polygons, 3, TessCombineCallback);
+                        tess.Tessellate(WindingRule.EvenOdd, ElementType.Polygons, 3);
                         var faceIndices = new List<int>(tess.ElementCount * 3);
                         var elements = tess.Elements;
                         var contourVerts = tess.Vertices;
